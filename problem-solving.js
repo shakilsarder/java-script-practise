ef48c0655ae1e5981adbd1cefdf0d3b98a23d710
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-06 00:21:14 +0600

    Reverse

--- problem-solving.js
+++ problem-solving.js
@@ -22,3 +22,14 @@ console.log(uniqueName);
 */
 
 //Explore string nature and reverse a string
+
+const greetings = 'Hello, how are you ?';
+
+function reverseString(text) {
+    let reverse = '';
+    for (const letter of text) {
+        reverse = letter + reverse;
+    }
+    return reverse;
+}
+console.log(reverseString(greetings));
