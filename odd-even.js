ccee46a06ec9c1df7200129a01c22248417b2137
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-05 10:26:39 +0600

    Odd even

---
+++ odd-even.js
@@ -0,0 +1,9 @@
+function numberChecker(number) {
+    if (number % 2 == 0) {
+        console.log(number, ' is the Even Number');
+    }
+    else {
+        console.log(number, ' is the Odd Number');
+    }
+}
+const input = numberChecker(100);
