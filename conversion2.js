68d6f522dd90d5c1a67bb15afa1674224b65a8dc
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-05 10:43:09 +0600

    km To Mile

--- conversion2.js
+++ conversion2.js
@@ -1,7 +1,21 @@
-// INCH to FEET
-function inchToFeet(inches) {
-    var feet = inches / 12;
-    return feet;
+// // INCH to FEET
+// function inchToFeet(inches) {
+//     var feet = inches / 12;
+//     return feet;
+// }
+// const input = inchToFeet(132);
+// console.log('My inches into feet = ', input);
+
+
+
+
+// km TO Mile 
+
+function kmToMile(km) {
+    var mile = km * 0.621371;
+    return mile;
 }
-const input = inchToFeet(132);
-console.log('My inches into feet = ', input);
+const input = kmToMile(300);
+console.log('My KM  into Mile  = ', input);
+
+
