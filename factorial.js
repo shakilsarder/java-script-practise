c30d0f27b69f7c36d531a110b28442c82988fd5f
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-05 10:55:24 +0600

    Factorial with while loop

--- factorial.js
+++ factorial.js
@@ -1,3 +1,4 @@
+/*
 function factorial(number) {
     let fact = 1;
     for (let i = 1; i <= number; i++) {
@@ -6,4 +7,31 @@ function factorial(number) {
     return fact;
 }
 const input = factorial(6);
+console.log('factorial is : ', input);
+*/
+
+/*
+function myFactorial(number) {
+    let fact = 1;
+    let i = 1;
+    while (i <= number) {
+        fact *= i;
+        i++;
+    }
+    return fact;
+}
+const input = myFactorial(7);
+console.log('factorial is : ', input);
+*/
+
+function myFactorial(number) {
+    let fact = 1;
+    let i = number;
+    while (i >= 1) {
+        fact *= i;
+        i--;
+    }
+    return fact;
+}
+const input = myFactorial(3);
 console.log('factorial is : ', input);
