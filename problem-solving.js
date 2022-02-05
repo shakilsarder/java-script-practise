b8d175ff9647de658229355cf84a067040601283
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-06 00:04:16 +0600

    Duplicate checker

---
+++ problem-solving.js
@@ -0,0 +1,24 @@
+//Duplicate checker 
+/*
+const names = ['Shakil', 'Akib', 'rabbi', 'alvi', 'Minahj', 'Jawad', 'rabbi', 'Akib'];
+
+function removeDuplicate(names) {
+    const unique = [];
+    // for (let i = 0; i < names.length; i++) {
+    //     const element = names[i];
+    //     //console.log(element);
+    // }
+    for (const element of names) {
+        console.log(element);
+        if (unique.indexOf(element) == -1) {
+            unique.push(element);
+        }
+    }
+    return unique;
+}
+const uniqueName = removeDuplicate(names);
+console.log(uniqueName);
+
+*/
+
+//Explore string nature and reverse a string
