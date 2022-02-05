927192e9433f25eed1aa3132f35b003ce120d5ec
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-05 10:19:22 +0600

    Yeap Year

--- conversion.js
+++ conversion.js
@@ -1,3 +1,5 @@
+/*****Leap Year*****
+
 function leapYear(year) {
     if (year % 4 == 0) {
         return true;
@@ -9,3 +11,17 @@ function leapYear(year) {
 }
 const input = leapYear(2020);
 console.log('This is the leap Year :', input);
+*/
+
+function leapYearChecker(year) {
+    //condtion to check leap year
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        console.log(year + ' is the leap Year');
+    }
+    else {
+        console.log(year + ' is Not the leap Year');
+    }
+}
+const input = leapYearChecker(2100);
+
+
