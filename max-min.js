7186ee7a57c138c0d20b1cfbc74802b460542ccb
Shakilk Sarder <shakil01748@hotmail.com>
2022-02-05 12:16:03 +0600

    max to min

--- max-min.js
+++ max-min.js
@@ -1,7 +1,12 @@
-// const business = 450;
-// const minsiter = 350;
+const a = 450;
+const b = 350;
+const c = 999;
+const max = Math.max(a, b, c);
+console.log('Maximum Number is ', max);
+const min = Math.min(a, b, c);
+console.log('Maximum Number is ', min);
 
-// Largest number checker 
+// Largest number checker
 /*
 function checker(num1, num2, num3) {
     if (num1 > num2 && num1 > num3) {
@@ -16,6 +21,9 @@ function checker(num1, num2, num3) {
 }
 checker(50, 155, 9); */
 
+
+// samllest number 
+/*
 function checker(num1, num2, num3) {
     if (num1 < num2 && num1 < num3) {
         console.log(num1, ' IS THE SMALEST NUMBER ');
@@ -27,4 +35,4 @@ function checker(num1, num2, num3) {
         console.log(num3, ' IS THE SMALEST NUMBER ');
     }
 }
-checker(500, 8, 99);
+checker(500, 8, 99); */
